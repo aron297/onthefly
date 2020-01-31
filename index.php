95e512c908fa4fa51b3cd1bebc6ffa81f37e0742
aron297 <edu.rocwb.nl>
2020-01-31 09:27:55 +0100

    init

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <title>vliegtuigen</title>
+</head>
+<body>
+    <h1>Home</h1>
+    <ul>
+    <li><a href="default.asp">Home</a></li>
+    <li><a href="news.asp">vliegtuigen</a></li>
+    <li><a href="contact.asp">Vluchten</a></li>
+    
+</ul>
+</body>
+</html>
